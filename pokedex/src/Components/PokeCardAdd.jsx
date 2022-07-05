d0cc9e3d4bdd183dff7af1021aa3380f1981bc79
Lucia Cufre Aman <luchicufre@hotmail.com>
2022-07-05 19:44:54 -0300

    home

--- pokedex/src/Components/PokeCardAdd.jsx
+++ pokedex/src/Components/PokeCardAdd.jsx
@@ -3,17 +3,17 @@ import { useNavigate } from "react-router-dom";
 import { goToDetailsPage } from "../Routes/coordinator";
 
 
-export default function PokeCardAdd() {
+export default function PokeCardAdd(props) {
     const navigate = useNavigate();
   
     return (
     
             <div className="cardFilho">
                 <div className="Card">
-                    <img className="imgCard"></img>
+                    <img src={props.url} alt={props.name} className="imgCard"></img>
                 </div>
                 <div className="containerBtn">
-                    <button className="btnCard">Adicionar</button>
+                    <button className="btnCard" onClick={props.addToCart}>Adicionar</button>
                     <button className="btnCard" onClick={() => goToDetailsPage(navigate)}>Ver Detalhe</button>
                 </div>
            
